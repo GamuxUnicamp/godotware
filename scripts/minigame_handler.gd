ebba41e111fac3c93e32154846e20103501494de
Thiago Amorim Mendonca Amendola <ra148062@purple.lab.ic.unicamp.br>
2017-08-25 22:20:25 -0300

    Created basic structure for minigames handling
    	- Created scenes/minigame_handling/minigame_handler.tscn and scripts/minigame_handler.gd which will handle minigames initialization and ending
    	- Created scenes/test_scene.tscn and scenes/test_scene.gd to test infrastructure model
    	- Created empty minigames/minigame1.tscn minigame node
    	- Created provisory project icon iconware.png

---
+++ scripts/minigame_handler.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+# This signal is called when the minigame finishes
+signal minigame_end(win)
+
+
+# This is the instance of the running minigame
+var running_minigame
+
+func _ready():
+	pass
+
+func _start_minigame(minigame_ref):
+	# Starts minigame
+	running_minigame = scene.instance()
+	add_child(running_minigame)
+	# Listen to its end
+	running_minigame.connect("minigame_end",self,"close_minigame")
+	
+func _close_minigame(win):
+	# Check for win status
+	if win:
+		print("ganhou")
+	else:
+		print("perdeu")
+	# Remove minigame from stage
+	remove_child(minigame)
